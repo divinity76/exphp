e44d23322aa4c48eec926c26049d492b936c4156
divinity76 <divinity76@gmail.com>
2016-04-03 19:00:05 +0200

    added rewind()

--- exphp.php
+++ exphp.php
@@ -87,5 +87,13 @@ class ex{
         }
         return $ret;
     }
+    static function rewind ( /*resource*/ $handle):bool {
+        $args=func_get_args();
+        $ret=call_user_func_array('rewind',$args);
+        if(false===$ret){
+            throw new RuntimeException('rewind() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
