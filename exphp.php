b282a3c40a46ed22f9a0a4d816e3c0c0fdbbfb52
divinity76 <divinity76@gmail.com>
2016-04-03 19:54:22 +0200

    added mkdir()

--- exphp.php
+++ exphp.php
@@ -187,6 +187,14 @@ class ex{
         }
         return $ret;
     }
+    static function mkdir ( string $pathname, int $mode = 0777, bool $recursive = false, /*resource*/ $context = null):bool {
+        $args=func_get_args();
+        $ret=call_user_func_array('mkdir',$args);
+        if(false===$ret){
+            throw new RuntimeException('mkdir() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 
 }
