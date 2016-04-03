d1c4de9ee4421e9b4d4f66acc945b83a662bc076
divinity76 <divinity76@gmail.com>
2016-04-03 18:30:30 +0200

    added touch()

--- exphp.php
+++ exphp.php
@@ -17,12 +17,19 @@ class ex{
         return $ret;
     }
     static function umask ( int $mask = null):int {
-    
         $ret=call_user_func_array('umask',func_get_args());
         if($mask!==null && $ret!==($mask & 0777)){
             throw new RuntimeException('umask() failed. tried to set umask to ( '.self::_return_var_dump(error_get_last($mask)).' & 0777) , but just managed to set it to '.self::_return_var_dump(error_get_last($ret)).'.   last error: '.self::_return_var_dump(error_get_last()));
         }
         return $ret;
     }
-	
+    static function touch ( string $filename, int $time = null /* = time() */ , int $atime = null):bool {
+        $args=func_get_args();
+        $ret=call_user_func_array('touch',$args);
+        if(false===$ret){
+            throw new RuntimeException('touch() failed.  last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
+
 }
