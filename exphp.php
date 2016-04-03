6642dfdfa30d6f03743000948da1f44b5e929b84
divinity76 <divinity76@gmail.com>
2016-04-03 18:30:50 +0200

    added tmpfile()

--- exphp.php
+++ exphp.php
@@ -31,5 +31,14 @@ class ex{
         }
         return $ret;
     }
+    static function tmpfile ( /*void*/ )/*:resource*/ {
+    
+        $args=func_get_args();
+        $ret=call_user_func_array('tmpfile',$args);
+        if(false===$ret){
+            throw new RuntimeException('tmpfile() failed. last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
