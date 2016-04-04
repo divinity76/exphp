036387e707ce10f8d95ee4bc4ccecfb8a3b2eb4f
divinity76 <divinity76@gmail.com>
2016-04-04 22:10:53 +0200

    added fileowner()

--- exphp.php
+++ exphp.php
@@ -389,5 +389,13 @@ class ex{
         }
         return $ret;
     }
-
+    static function fileowner ( string $filename):int {
+        $args=func_get_args();
+        $ret=call_user_func_array('fileowner',$args);
+        if(false===$ret){
+            throw new RuntimeException('fileowner() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
+ 
 }
