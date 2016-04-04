437aff1136ee88c4b4b2a3f711430ea6927ee382
divinity76 <divinity76@gmail.com>
2016-04-04 22:01:25 +0200

    added fpassthru

--- exphp.php
+++ exphp.php
@@ -336,5 +336,13 @@ class ex{
         }
         return $ret;
     }
+    static function fpassthru ( /*resource*/ $handle):int {
+        $args=func_get_args();
+        $ret=call_user_func_array('fpassthru',$args);
+        if(false===$ret){
+            throw new RuntimeException('fpassthru() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
