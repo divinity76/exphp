45961cbf920bb74201b2e11137b509cd83407663
divinity76 <divinity76@gmail.com>
2016-04-04 23:00:01 +0200

    file_get_contents

--- exphp.php
+++ exphp.php
@@ -466,5 +466,13 @@ class ex{
         }
         return $ret;
     }
+    static function file_get_contents ( string $filename, bool $use_include_path = false, /*resource*/ $context = null, int $offset = 0, int $maxlen = null):string {
+        $args=func_get_args();
+        $ret=call_user_func_array('file_get_contents',$args);
+        if(false===$ret){
+            throw new RuntimeException('file_get_contents() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
