a131000547f0be54d8939a049e688daada5da188
divinity76 <divinity76@gmail.com>
2016-04-04 22:07:29 +0200

    added filesize()

--- exphp.php
+++ exphp.php
@@ -373,5 +373,13 @@ class ex{
         }
         return $ret;
     }
+    static function filesize ( string $filename):int {
+        $args=func_get_args();
+        $ret=call_user_func_array('filesize',$args);
+        if(false===$ret){
+            throw new RuntimeException('filesize() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
