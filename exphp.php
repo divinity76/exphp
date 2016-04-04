d72f4912dd4c48d0fdd46e432ea697b43d9eb386
divinity76 <divinity76@gmail.com>
2016-04-04 22:22:20 +0200

    fileatime

--- exphp.php
+++ exphp.php
@@ -429,5 +429,13 @@ class ex{
         }
         return $ret;
     }
+    static function fileatime ( string $filename):int {
+        $args=func_get_args();
+        $ret=call_user_func_array('fileatime',$args);
+        if(false===$ret){
+            throw new RuntimeException('fileatime() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
