a6580a258f7f8dfbf7d5206f98f8af8bd830b3f6
divinity76 <divinity76@gmail.com>
2016-04-04 23:44:48 +0200

    diskfreespace

--- exphp.php
+++ exphp.php
@@ -544,6 +544,14 @@ class ex{
         }
         return $ret;
     }
+    static function diskfreespace ( string $directory):float {
+        $args=func_get_args();
+        $ret=call_user_func_array('diskfreespace',$args);
+        if(false===$ret){
+            throw new RuntimeException('diskfreespace() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 
 }
