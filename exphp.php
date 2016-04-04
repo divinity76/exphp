7ce569bb9881b4bc67cf01b888703e73573fea4a
divinity76 <divinity76@gmail.com>
2016-04-04 22:02:39 +0200

    added fopen()

--- exphp.php
+++ exphp.php
@@ -344,5 +344,13 @@ class ex{
         }
         return $ret;
     }
+    static function fopen ( string $filename, string $mode, bool $use_include_path = false, /*resource*/ $context = null)/*:resource*/ {
+        $args=func_get_args();
+        $ret=call_user_func_array('fopen',$args);
+        if(false===$ret){
+            throw new RuntimeException('fopen() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
