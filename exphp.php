7759497e9b4f4fd31c7f921038373fab111d84c9
divinity76 <divinity76@gmail.com>
2016-04-30 06:21:58 +0200

    getcwd()

--- exphp.php
+++ exphp.php
@@ -612,5 +612,13 @@ class ex{
         }
         return $ret;
     }
+    static function getcwd ( /*void*/ ):string {
+        $args=func_get_args();
+        $ret=call_user_func_array('getcwd',$args);
+        if(false===$ret){
+            throw new RuntimeException('getcwd() failed.   last error: '.self::_return_var_dump(error_get_last()));
+        }
+        return $ret;
+    }
 
 }
