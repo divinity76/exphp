8a4d29644b97fa2fb438ca09ccb020caa2ef8f1e
divinity76 <divinity76@gmail.com>
2016-05-26 11:32:28 +0200

    socket_setopt

--- exphp.php
+++ exphp.php
@@ -868,6 +868,14 @@ class ex {
 		}
 		return $ret;
 	}
+	static function socket_setopt ( /*resource*/ $socket, int $level, int $optname, /*mixed*/ $optval): bool {
+		$args = func_get_args ();
+		$ret = call_user_func_array ( 'socket_setopt', $args );
+		if (false === $ret) {
+			throw new RuntimeException ( 'socket_setopt() failed.   last error: ' . self::_return_var_dump ( error_get_last () ) . '. socket_last_error: ' . self::_return_var_dump ( socket_last_error ( $socket ) ) . '. socket_strerror: ' . self::_return_var_dump ( socket_strerror ( socket_last_error ( $socket ) ) ) );
+		}
+		return $ret;
+	}
 	
 	/* </sockets> */
 }
