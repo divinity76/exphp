e6ace4007840bae54b71a5544876627ce26d3fdc
divinity76 <divinity76@gmail.com>
2016-05-26 11:00:17 +0200

    socket_import_stream

--- exphp.php
+++ exphp.php
@@ -749,6 +749,14 @@ class ex {
 		}
 		return $ret;
 	}
+	static function socket_import_stream ( /*resource*/ $stream)/*:resource*/ {
+		$args = func_get_args ();
+		$ret = call_user_func_array ( 'socket_import_stream', $args );
+		if (false === $ret || null === $ret) {
+			throw new RuntimeException ( 'socket_import_stream() failed.   last error: ' . self::_return_var_dump ( error_get_last () ) . '. socket_last_error: ' . self::_return_var_dump ( socket_last_error ( $socket ) ) . '. socket_strerror: ' . self::_return_var_dump ( socket_strerror ( socket_last_error ( $socket ) ) ) );
+		}
+		return $ret;
+	}
 	
 	/* </sockets> */
 }
